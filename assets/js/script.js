6eb85bbb812a085fa073299e57c5ee71d12990b4
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-06-02 13:13:43 -0500

    Hyperfocused most of it. Added ordinals on time with advanced format plugins and time mgmt with dayjs utc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.\
+const td = new Date()
 
-$(window).on('load', function () {
-    function createSchedule(currentHour) {
-        for(index = 0; index < 11; index++){
-            if ((index++ / 2) > hour){
-                timeRelevance = "future"
-            }
-                hourDiv = $('<div id="hour-' + index + '"' + ' class="row time-block ' + timeRelevance + '">')
-                <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-                <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-                <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-                  <i class="fas fa-save" aria-hidden="true"></i>
-                </button>
-              </div>}
+
+const hourContainer = $("#time-container")
+$(window).on('load', function() {
+    function createSchedule(currentHour, hourAM) {
+        for(index = 0; index < 24; index++){
+          let hour = index % 12; 
+          hour = (hour === 0) ? 12 : hour; // to make 12 AM and 12 PM instead of 0
+          let meridiem = (index < 12 || index === 24) ? "AM" : "PM";
+          let timeVal = hour + meridiem;
+            
+          if (index > currentHour){
+            timeRelevance = "future"
+          }
+          else if (index < currentHour){
+            timeRelevance = "past"
+          }
+          else {
+            timeRelevance = "present"
+          }
+          let hourDiv = $('<div id="hour-' + timeVal + '"' + ' class="row time-block ' + timeRelevance + '"></div>')
+          let timeText = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + timeVal + '</div>')
+          let inputArea = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>')
+          let saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
+          let saveImage = $('<i class="fas fa-save" aria-hidden="true"></i>')
+          hourDiv.append(timeText, inputArea, saveButton.append(saveImage))
+          $(hourContainer).append(hourDiv)
         }
+      }
+      const currentDate = dayjs().format("dddd, MMMM Do")
+      var currentTime = dayjs().hour()
+      $("#current-day").text(currentDate)
+      createSchedule(currentTime)
+      
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -35,4 +55,3 @@ $(window).on('load', function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
-  
