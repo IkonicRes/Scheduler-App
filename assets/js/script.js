df08d7cd6d37fc93152385fe0eb5378ce7d9d8fb
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-06-03 11:16:08 -0500

    So very close to fixing breaking bug, should be more calendar oreinted and ordered.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(window).on('load', function() {
   const td = new Date()
   const hourContainer = $("#time-container")
   let scheduleExists = (localStorage.getItem('schedule') !== null)
   let currentTime = dayjs().hour()
 
+  function createEmptySchedule() {
+    let emptySchedule = [];
+    for (let index = 0; index < 24; index++) {
+      emptySchedule[index] = index + "%"
+    }
+    return emptySchedule;
+  }
   function createSchedule(currentHour) {
     let currentSchedule = localStorage.getItem('schedule')
-    let startHour = (currentHour - 9 + 24) % 24;
+    let startHour = (currentHour - 9 + 24) % 24
     if (startHour < 0) {
-      startHour += 24;
+      startHour += 24
     }
     if (scheduleExists) {
       currentSchedule = JSON.parse(localStorage.getItem('schedule'))
@@ -43,78 +47,84 @@ $(window).on('load', function() {
     }
   }
 
-function createHourDiv(hour) {
-  let hourVal = hour % 12 === 0 ? 12 : hour % 12 // Convert to 12-hour format
-  let meridiem = hour < 12 || hour === 24 ? "AM" : "PM" // Determine AM or PM
-  let timeVal = hourVal + meridiem;
-  let hourDiff = (hour - currentTime + 24) % 24 // Difference taking into account overflow at 24
-  let timeRelevance = ""
+  function createHourDiv(hour) {
+    let hourVal = hour % 12 === 0 ? 12 : hour % 12 // Convert to 12-hour format
+    let meridiem = hour < 12 || hour === 24 ? "AM" : "PM" // Determine AM or PM
+    let timeVal = hourVal + meridiem;
+    let hourDiff = (hour - currentTime + 24) % 24 // Difference taking into account overflow at 24
+    let timeRelevance = ""
 
-  if (hourDiff < 0) {
-    timeRelevance = "past"
-  } else if (hourDiff === 0) {
-    timeRelevance = "present"
-  } else {
-    timeRelevance = "future"
+    if (hourDiff < 0) {
+      timeRelevance = "past"
+    } else if (hourDiff === 0) {
+      timeRelevance = "present"
+    } else {
+      timeRelevance = "future"
+    }
+
+    let hourDiv = $('<div id="hour-' + timeVal + '"' + ' class="row time-block ' + timeRelevance + '"></div>')
+    let timeText = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + timeVal + '</div>')
+    let inputArea = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>')
+    let saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
+    let saveImage = $('<i class="fas fa-save" aria-hidden="true"></i>')
+
+    hourDiv.append(timeText, inputArea, saveButton.append(saveImage))
+
+    return hourDiv;
   }
 
-  let hourDiv = $('<div id="hour-' + timeVal + '"' + ' class="row time-block ' + timeRelevance + '"></div>')
-  let timeText = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + timeVal + '</div>')
-  let inputArea = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>')
-  let saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
-  let saveImage = $('<i class="fas fa-save" aria-hidden="true"></i>')
-
-  hourDiv.append(timeText, inputArea, saveButton.append(saveImage))
-
-  return hourDiv;
-}
-
-
-  let msUntilNextHour = 60 * 60 * 1000 - (dayjs().valueOf() % (60 * 60 * 1000))
-  setTimeout(function() {
-      updateHour()
-      setInterval(updateHour, 60 * 60 * 1000)
-  })
-
-  function updateHour() {
-    currentTime = (currentTime + 1) % 24
-    let newHour = (currentTime + 19) % 24
-    let newHourDiv = createHourDiv(newHour)
-    // Fade out the first child and remove it after it's hidden
-    hourContainer.children().first().fadeOut(function() { $(this).remove()})
-    // Animate the change in margin-top
-    hourContainer.animate({ marginTop: "-=100px" }, 1000, function() {
-    hourContainer.css({ marginTop: "0px" })
-    hourContainer.children().first().remove() // Remove the first hour after the animation finishes
-})
-     // Append the new child and fade it in
-     hourContainer.append(newHourDiv)
-     newHourDiv.hide().fadeIn()
-  }
-
-  setInterval(updateHour, 60 * 60 * 1000)
-
-  function updateSchedule(newSchedule){
-    if (scheduleExists){
+  function updateSchedule(newSchedule) {
+    if (scheduleExists) {
       localStorage.clear("schedule")
     }
-    localStorage.setItem("schedule", JSON.stringify(newSchedule))
+    const formattedSchedule = newSchedule.map((scheduleItem, hourIndex) => {
+      const meridiem = hourIndex < 12 || hourIndex === 24 ? "AM" : "PM"
+      const currentDate = dayjs().format("MMM Do")
+      return meridiem + "/" + currentDate + "%" + scheduleItem
+    })
+    localStorage.setItem("schedule", JSON.stringify(formattedSchedule))
   }
-  
-  const currentDate = dayjs().format("dddd, MMMM Do")
-  
-  $("#current-day").text(currentDate)
-  createSchedule(currentTime)
-  allSaveBtn = $(".saveBtn")
-  
+
+  const currentDate = dayjs().format("MMM Do")
+  let schedule = JSON.parse(localStorage.getItem("schedule"))
+
+  if (!schedule) {
+    schedule = createEmptySchedule()
+    updateSchedule(schedule)
+  }
+
+  let allSaveBtn = $(".saveBtn")
   $(allSaveBtn).on("click", function() {
     let tScheduleArr = []
     $('.time-block').each(function() {
-      let id = $(this).attr('id');
+      let id = $(this).attr('id')
       let hourIndex = parseInt(id.split("-").pop()) // Extract the hour index from the id
       let inputAreaText = $(this).children('textarea.description').val()
       tScheduleArr[hourIndex] = hourIndex + "%" + inputAreaText
     })
     updateSchedule(tScheduleArr)
   })
+
+  let msUntilNextHour = 60 * 60 * 1000 - (dayjs().valueOf() % (60 * 60 * 1000))
+  setTimeout(function() {
+    updateHour()
+    setInterval(updateHour, 60 * 60 * 1000)
+  })
+
+  function updateHour() {
+    currentTime = (currentTime + 1) % 24
+    let newHour = (currentTime + 15) % 24
+    let newHourDiv = createHourDiv(newHour)
+    // Fade out the first child and remove it after it's hidden
+    hourContainer.children().first().fadeOut(function() { $(this).remove(); })
+    // Animate the change in margin-top
+    hourContainer.animate({ marginTop: "-=100px" }, 1000, function() {
+      hourContainer.css({ marginTop: "0px" })
+      hourContainer.children().first().remove(); // Remove the first hour after the animation finishes
+    })
+    // Append the new child and fade it in
+    hourContainer.append(newHourDiv)
+    newHourDiv.hide().fadeIn()
+  }
+  setInterval(updateHour, 60 * 60 * 1000)
 })
