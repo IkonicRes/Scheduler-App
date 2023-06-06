e3cb781306e88e4b7817d8512fb00ec32e47e3c2
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-06-05 19:19:43 -0500

    Finished adding the schedule saving to localstorage, restructured a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,126 +1,111 @@
 $(window).on('load', function() {
   const hourContainer = $("#time-container")
-  let scheduleExists = (localStorage.getItem('schedule') !== null)
-  let currentTime = dayjs().hour()
-  const currentDate = dayjs().format("dddd, MMMM Do")
-  $("#current-day").text(currentDate)
+  let currentDay = dayjs()
+  let currentTime = currentDay.hour()
+  let navEl = $('<nav class="center"></nav>')
+  $('header').append(navEl)
+  // Adding a next and previous day button
+  let nextButton = $('<button class="btn btn-primary" id="next-day">Next Day</button>')
+  let prevButton = $('<button class="btn btn-primary" id="prev-day">Previous Day</button>')
+
+  $(navEl).append(prevButton, nextButton)
+
+  // Handler for next day button
+  $('#next-day').on('click', function() {
+    saveSchedule();
+    changeDay(1);
+  });
+
+  // Handler for previous day button
+  $('#prev-day').on('click', function() {
+    saveSchedule();
+    changeDay(-1);
+  });
+
+  function changeDay(days) {
+    currentDay = currentDay.add(days, 'day');
+    renderDay();
+  }
+
+  function renderDay() {
+    const schedule = getScheduleForDay();
+    $("#current-day").text(currentDay.format("dddd, MMMM Do"))
+    renderSchedule(schedule)
+  }
+
+  function getScheduleForDay() {
+    const allSchedule = JSON.parse(localStorage.getItem('schedule')) || {};
+    const daySchedule = allSchedule[currentDay.format("YYYY-MM-DD")] || createEmptySchedule();
+    return daySchedule;
+  }
 
   function createEmptySchedule() {
-    let emptySchedule = [];
+    let emptySchedule = {};
     for (let index = 0; index < 24; index++) {
-      emptySchedule[index] = index + "%"
+      emptySchedule[index] = "";
     }
     return emptySchedule;
   }
 
-  function createSchedule(currentHour) {
-    let currentSchedule = localStorage.getItem('schedule')
-    let startHour = 0
-    if (scheduleExists) {
-      currentSchedule = JSON.parse(localStorage.getItem('schedule'))
+  function renderSchedule(schedule) {
+    hourContainer.empty()
+    for (let index = 0; index < 24; index++) {
+        let hour = index % 12
+        hour = (hour === 0) ? 12 : hour
+        let meridiem = (index < 12 || index === 24) ? "AM" : "PM"
+        let timeVal = hour + meridiem;
+        
+        // Add classes for past, present, and future hours
+        let timeRelevance = ""
+        if (index < currentTime) {
+            timeRelevance = "past"
+        } else if (index > currentTime) {
+            timeRelevance = "future"
+        } else {
+            timeRelevance = "present"
+        }
+        
+        let hourDiv = $('<div id="hour-' + index + '"' + ' class="row time-block ' + timeRelevance + '"></div>')
+        let timeText = $('<div id="time-text-box" class="col-2 col-md-1 hour text-center py-3">' + timeVal + '</div>')
+        let inputArea = $('<textarea class="col-8 col-md-10 description" rows="3">' + (schedule[index] || "") + '</textarea>')
+        let saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
+        let saveImage = $('<i class="fas fa-save" aria-hidden="true"></i>')
+
+        hourDiv.append(timeText, inputArea, saveButton.append(saveImage))
+        hourContainer.append(hourDiv)
     }
-    for (index = 0; index <= 23; index++) {
-      let altIndex = (startHour + index) % 24
-      let hour = altIndex % 12
-      hour = (hour === 0) ? 12 : hour
-      let meridiem = (altIndex < 12 || altIndex === 24) ? "AM" : "PM"
-      let timeVal = hour + meridiem;
-      let tLabelText = ""
-      if (scheduleExists && currentSchedule && currentSchedule[altIndex]) {
-        tLabelText = currentSchedule[altIndex].split("%").pop()
-      }
+}
 
-      let timeRelevance = ""
-      if (index < 9) timeRelevance = "past"
-      else if (index > 9 && index !== 25) timeRelevance = "future"
-      else timeRelevance = "present"
-
-      let hourDiv = $('<div id="hour-' + altIndex + '"' + ' class="row time-block ' + timeRelevance + '"></div>')
-      let timeText = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + timeVal + '</div>')
-      let inputArea = $('<textarea class="col-8 col-md-10 description" rows="3">' + tLabelText + '</textarea>')
-      let saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
-      let saveImage = $('<i class="fas fa-save" aria-hidden="true"></i>')
-
-      hourDiv.append(timeText, inputArea, saveButton.append(saveImage))
-      $(hourContainer).append(hourDiv)
-    }
-  }
-
-  function createHourDiv(hour) {
-    
-    let hourVal = hour % 12 === 0 ? 12 : hour % 12 // Convert to 12-hour format
-    let meridiem = hour < 12 || hour === 24 ? "AM" : "PM" // Determine AM or PM
-    let timeVal = hourVal + meridiem;
-    let hourDiff = (hour - currentTime + 24) % 24 // Difference taking into account overflow at 24
-    let timeRelevance = ""
-
-    if (hourDiff < 0) {
-      timeRelevance = "past"
-    } else if (hourDiff === 0) {
-      timeRelevance = "present"
-    } else {
-      timeRelevance = "future"
-    }
-
-    let hourDiv = $('<div id="hour-' + timeVal + '"' + ' class="row time-block ' + timeRelevance + '"></div>')
-    let timeText = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + timeVal + '</div>')
-    let inputArea = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>')
-    let saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
-    let saveImage = $('<i class="fas fa-save" aria-hidden="true"></i>')
-
-    hourDiv.append(timeText, inputArea, saveButton.append(saveImage))
-
-    return hourDiv;
-  }
-
-  function updateSchedule(newSchedule) {
-    if (scheduleExists) {
-      localStorage.clear("schedule")
-    }
-    const formattedSchedule = newSchedule.map((scheduleItem, hourIndex) => {
-      const meridiem = hourIndex < 12 || hourIndex === 24 ? "AM" : "PM"
-      const currentDate = dayjs().format("MMM Do")
-      return hourIndex + meridiem + "/" + currentDate + "%" + scheduleItem
-    })
-    localStorage.setItem("schedule", JSON.stringify(formattedSchedule))
-  }
-
-  let schedule = JSON.parse(localStorage.getItem("schedule"))
-
-  if (!schedule) {
-    schedule = createEmptySchedule()
-    updateSchedule(schedule)
-  }
-  createSchedule(currentTime)
-  let allSaveBtn = $(".saveBtn")
-  $(allSaveBtn).on("click", function() {
-    let tScheduleArr = []
+  function saveSchedule() {
+    let schedule = {};
     $('.time-block').each(function() {
       let id = $(this).attr('id')
-      let hourIndex = parseInt(id.split("-").pop()) // Extract the hour index from the id
+      let hourIndex = parseInt(id.split("-").pop()) 
       let inputAreaText = $(this).children('textarea.description').val()
-      tScheduleArr[hourIndex] = inputAreaText
+      schedule[hourIndex] = inputAreaText
     })
-    console.log(tScheduleArr)
-    updateSchedule(tScheduleArr)
+
+    const allSchedule = JSON.parse(localStorage.getItem('schedule')) || {}
+    allSchedule[currentDay.format("YYYY-MM-DD")] = schedule;
+    localStorage.setItem('schedule', JSON.stringify(allSchedule))
+  }
+
+  // Save button handler
+  $(document).on('click', '.saveBtn', function() {
+    saveSchedule();
   })
 
-  
-  updateHour()
-
+  // Update hour every hour
   function updateHour() {
-    if (dayjs().hour() != currentTime) {
-      // Fade out the first child and remove it after it's hidden
-      hourContainer.children().first().fadeOut(function() { $(this).remove(); })
-    
-      currentTime = (currentTime + 1) % 24
-      let newHourDiv = createHourDiv((currentTime + 15) % 24)
-      // Append the new child and fade it in
-      hourContainer.append(newHourDiv)
-      newHourDiv.hide().fadeIn()
+    let newDay = dayjs();
+    if (newDay.format("YYYY-MM-DD") !== currentDay.format("YYYY-MM-DD")) {
+      currentDay = newDay;
+      renderDay();
     }
-    let msUntilNextHour = 60 * 60 * 1000 - (dayjs().valueOf() % (60 * 60 * 1000))
+    let msUntilNextHour = 60 * 60 * 1000 - (newDay.valueOf() % (60 * 60 * 1000))
     setTimeout(updateHour, msUntilNextHour)  
   }
 
+  renderDay()
+  updateHour()
 })
